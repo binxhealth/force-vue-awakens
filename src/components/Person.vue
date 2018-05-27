db1d93315afb43cdf05eaba5322a2d589528f74d
Stephanie Frankian <sfrankia@wellesley.edu>
2018-05-27 19:59:07 -0400

    Changed title of profile page from 'Person n' to name retrived from API (issue #3).

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Person {{ $route.params.id }}
+    <h1 @change="updatePersonAttributes">{{ attributes.name }} </h1>
   </div>
 </template>
 
@@ -11,9 +11,11 @@ import store from '@/store'
 export default {
   async beforeRouteEnter (to, from, next) {
     const action = await store.dispatch('people/getPerson', to.params.id)
+    const json = await action.json()
     if(action.ok) {
-      console.log(action.json())
-      next()
+      console.log(json.name)
+      //attributes.name = json.name
+      next(vm => vm.updatePersonAttributes(json))
     } else {
       // If the response from the API is not ok, then we redirect to 404 page 
       next (
@@ -24,25 +26,36 @@ export default {
     }
   },
   async beforeRouteUpdate (to, from, next) {
-   const action = await store.dispatch('people/getPerson', to.params.id)
-   if(action.ok) {
-    next()
-   } else {
-     next(
-       {
-         path: '/'
-       }
-     )
-     this.$notify({
+    const action = await store.dispatch('people/getPerson', to.params.id)
+    const json = await action.json()
+    if(action.ok) {
+      console.log('Name of person: ' + json.name)
+      //attributes.name = json.name
+      next(vm => vm.updatePersonAttributes(json))
+    } else {
+    next({
+      path: '/'
+    })
+    this.$notify({
       group: 'notifs',
       title: 'Error',
       type: 'warn',
       text: 'Person ' + to.params.id + ' not found.'
-     })
+    })
    }
   },
+  data: function() {
+    return {
+      attributes: {
+        name: ''
+      }
+    }
+  },
   methods: {
-    ...mapActions('getPerson', ['people/getPerson'])
+    ...mapActions('getPerson', ['people/getPerson']),
+    updatePersonAttributes(json) {
+      this.attributes.name = json.name
+    },
   },
   computed: {
     ...mapGetters('people', ['person'])
