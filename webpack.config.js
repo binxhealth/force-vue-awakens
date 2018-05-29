3f6a314646cedcb139792a825beb0ee416e5ff5e
Jake Kaplan <jakekaplan@Jakes-MacBook-Pro.local>
2018-05-29 19:57:43 -0400

    #20 part 1, lint Vue SFCs

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
   },
   module: {
     rules: [
+      { enforce: "pre", test: /\.(js|vue)$/, exclude: /node_modules/, loader: "eslint-loader"},
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.vue$/, loader: 'vue-loader' }
     ]
