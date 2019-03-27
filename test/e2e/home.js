9240622bdf6d7014ad4a1c51fbb4576cbc1af69f
Ian Walter <public@iankwalter.com>
2019-03-27 12:00:18 -0400

    Adding WebdriverIO

---
+++ test/e2e/home.js
@@ -0,0 +1,7 @@
+describe('Home', () => {
+  it('should list characters', async () => {
+    await browser.url('/')
+    const title = await browser.getTitle()
+    expect(title).toBe('Force Vue Awakens')
+  })
+})
