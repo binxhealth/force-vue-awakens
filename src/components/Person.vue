82206b27b73bef411b22f302a84397774a29f096
Ty Holland <tyronehus@gmail.com>
2019-04-16 22:18:57 -0400

    Issue #3
    made changes to the person component page

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,25 +1,44 @@
 <template>
   <div>
-    Person {{ $route.params.id }}
+    <div v-for="(person, id) in results">
+      <div v-if="$route.params.id == id">
+        <div>
+          <a href="../">&laquo; Back</a>
+        </div>
+        <h2>{{ person.name }}</h2>
+        <div style="float: left; width: 20%">
+          <img :src="`https://starwars-visualguide.com/assets/img/characters/${id + 1}.jpg`" width="100%" />
+        </div>
+        <ul style="float: left;">
+          <li><strong>DOB:</strong> {{ person.birth_year }}</li>
+          <li><strong>Gender:</strong> {{ person.gender }}</li>
+          <li><strong>Height:</strong> {{ person.height }}</li>
+          <li><strong>Weight:</strong> {{ person.mass }}</li>
+          <li><strong>Hair Color:</strong> {{ person.hair_color }}</li>
+          <li><strong>Eye Color:</strong> {{ person.eye_color }}</li>
+          <li><strong>Skin Color:</strong> {{ person.skin_color }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapState } from 'vuex'
 
 import store from '@/store'
 
 export default {
   async beforeRouteEnter (to, from, next) {
-    await store.dispatch('people/get', to.params.id)
+    await store.dispatch('people/list', to.params.id)
     next()
   },
   async beforeRouteUpdate (to, from, next) {
-    await store.dispatch('people/get', to.params.id)
+    await store.dispatch('people/list', to.params.id)
     next()
   },
   computed: {
-    ...mapGetters('people', ['person'])
+    ...mapState('people', ['results'])
   }
 }
 </script>
