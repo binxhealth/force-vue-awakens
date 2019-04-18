7ea5a48fe9f36aadb374395ff604e0ce462e7d08
Alex Pihlak <alex@Alexs-MacBook-Air-2.local>
2019-04-18 11:45:58 -0400

    first commit

--- test/e2e/home.js
+++ test/e2e/home.js
@@ -2,6 +2,9 @@ describe('Home', () => {
   it('should list characters', async () => {
     await browser.url('/')
     const title = await browser.getTitle()
+//    const charachters = await browser.getH2()
     expect(title).toBe('Force Vue Awakens')
+//    expect(charachters).toMatch('People')
+    expect().toBe()
   })
 })
