f0658692cf4f3a526435372c0444ce8f5dbd48df
Arlo Duff <aduff@nyu.edu>
2019-04-18 12:26:10 -0400

    Added tests to change for correct number of cards, names, and images

--- test/e2e/home.js
+++ test/e2e/home.js
@@ -3,5 +3,26 @@ describe('Home', () => {
     await browser.url('/')
     const title = await browser.getTitle()
     expect(title).toBe('Force Vue Awakens')
-  })
-})
+  });
+
+  it('should list 10 people', async () => {
+	await browser.pause(4000);
+	const cards = await $$('a.card');
+	expect(cards.length).toBe(10);
+  });
+
+  it('should list a name for each person', async () => {
+	const names = await $$('.card .card-title');
+	expect(names.length).toBe(10);
+  });
+
+  it('should display an image for each person', async () => {
+	const images = await $$('.card img');
+	expect(images.length).toBe(10);
+
+	for(let img of images) {
+		const isDisplayed = await img.isDisplayed();
+		expect(isDisplayed).toBe(true);
+	}
+	});
+});
