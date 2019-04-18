5b3f1442a19c5d281668973c1e32d4f4f1f99673
Alex Pihlak <alex@Alexs-MacBook-Air-2.local>
2019-04-18 12:46:49 -0400

    base-layer sudocode is done, nothing is functional

--- test/e2e/clicker_test.js
+++ test/e2e/clicker_test.js
@@ -3,7 +3,7 @@
 describe('Clicker', () => {
   it('should click on each Person listed in the index', async () => {
     await browser.url('/')
-    for i in {
+    for i in index{
     WebElement mapObject = driver.findElement(By.xpath("//*[href()='#/people/']/d*"));
         Actions builder = new Actions(driver);
         builder.contextClick(mapObject).perform();
--- test/e2e/home.js
+++ test/e2e/home.js
@@ -1,15 +1,24 @@
+// a decent majority of this is sudocode, needs to be flushed
+// out in correct terminology relating to Jest, Jasmine and Selenium
 describe('Home', () => {
   it('should list ten characters', async () => {
     await browser.url('/')
     const title = await browser.getTitle()
+    const imageData = await browser.getHref('datatype')
+    const imageLink = await browser.getHref()
 //    const charachters = await browser.getH2()
     expect(title).toBe('Force Vue Awakens')
 //    expect(charachters).toMatch('People')
+    expect(imageData).toBe('.jpeg')
+    expect(imageLink).toBe('https://*')
   })
 })
 
 describe('Charachter', () => {
   it('should have own image, name and link', async () => {
+    const imageData = await browser.get()
+    const nameData = await browser.getBody(h2)
+    const imageLink = await browser.getHref()
     await browser.url('/')
     await page.index()
     expect().toBe()
