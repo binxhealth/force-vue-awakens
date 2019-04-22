2e1344f07f590ba573bec1bf6745826017a6abf6
Alex Pihlak <amp1291@gmail.com>
2019-04-22 17:02:50 -0400

    finished psudeocode and high-level architecture

--- test/e2e/clicker_test.js
+++ test/e2e/clicker_test.js
@@ -1,12 +1,19 @@
-//Frankenstien code that I am currently working on
-//to get a Selenium/Jest test to click items on the index page
+// a majority of this is sudocode, that needs to be flushed
+// out in correct terminology relating to Jest, Jasmine and Selenium.
+// This is to get an idea of where I am going, as getting working code in
+// a foreign tech stack will take some time.
+// continuing to learn the tech stack as-of 04/22/19
+
+require('selenium-webdriver');
+
 describe('Clicker', () => {
   it('should click on each Person listed in the index', async () => {
     await browser.url('/')
     for i in index{
     WebElement mapObject = driver.findElement(By.xpath("//*[href()='#/people/']/d*"));
         Actions builder = new Actions(driver);
-        builder.contextClick(mapObject).perform();
+        builder.element = driver.find_element :xpath, '//input[@name="q"]'
+        element.click();();
     }
   })
 })
--- test/e2e/home.js
+++ test/e2e/home.js
@@ -1,26 +1,25 @@
-// a decent majority of this is sudocode, needs to be flushed
-// out in correct terminology relating to Jest, Jasmine and Selenium
+// a majority of this is sudocode, that needs to be flushed
+// out in correct terminology relating to Jest, Jasmine and Selenium.
+// This is to get an idea of where I am going, as getting working code in
+// a foreign tech stack will take some time.
+// continuing to learn the tech stack as-of 04/22/19
+
 describe('Home', () => {
   it('should list ten characters', async () => {
     await browser.url('/')
     const title = await browser.getTitle()
-    const imageData = await browser.getHref('datatype')
-    const imageLink = await browser.getHref()
-//    const charachters = await browser.getH2()
-    expect(title).toBe('Force Vue Awakens')
-//    expect(charachters).toMatch('People')
-    expect(imageData).toBe('.jpeg')
-    expect(imageLink).toBe('https://*')
   })
 })
 
 describe('Charachter', () => {
   it('should have own image, name and link', async () => {
-    const imageData = await browser.get()
+    const imageData = await browser.getElementById("href")
+    const imageLink = await browser.getElementById("src")
     const nameData = await browser.getBody(h2)
-    const imageLink = await browser.getHref()
     await browser.url('/')
     await page.index()
+    expect(imageData).toBe('.jpeg')
+    expect(imageLink).toBe('https://*')
     expect().toBe()
   })
 })
