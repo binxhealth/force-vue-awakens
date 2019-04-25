c7e40178fbe0edbab7cf91528110d448c20c57ef
FrankCardillo <facardillo91@gmail.com>
2019-04-25 11:56:03 -0400

    #20 fxing linter not recognizing the vue lazy load component import syntax

--- src/router.js
+++ src/router.js
@@ -4,10 +4,8 @@ import VueRouter from 'vue-router'
 // Instruct Vue to use the router plugin.
 Vue.use(VueRouter)
 
-/* eslint-disable */ 
 const Home = () => import('@/components/Home')
 const Person = () => import('@/components/Person')
-/* eslint-enable */ 
 
 // Export a new Vue Router instance to be used in the application.
 export default new VueRouter({
