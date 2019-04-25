be4ee601ef271e972ae721e75b12f79311b7b78c
FrankCardillo <facardillo91@gmail.com>
2019-04-25 14:14:01 -0400

    #12 WIP :( Working to mock the state so I could get the component to render the swapi data and test the snapshot against that.

--- test/e2e/home.js
+++ test/e2e/home.js
@@ -1,7 +1,7 @@
-describe('Home', () => {
-  it('should list characters', async () => {
-    await browser.url('/')
-    const title = await browser.getTitle()
-    expect(title).toBe('Force Vue Awakens')
-  })
-})
+// describe('Home', () => {
+//   it('should list characters', async () => {
+//     await browser.url('/')
+//     const title = await browser.getTitle()
+//     expect(title).toBe('Force Vue Awakens')
+//   })
+// })
--- test/unit/home.test.js
+++ test/unit/home.test.js
@@ -1,14 +1,40 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import fs from 'fs';
+import path from 'path';
 
-import store from '@/store'
+// import store from '@/store'
 import Home from '@/components/Home'
 
 const localVue = createLocalVue()
 
 localVue.use(Vuex)
 
-test('Home renders correctly', () => {
+test('Home renders correctly', (done) => {
+  const rawData = fs.readFileSync(path.resolve(__dirname, '../data/swapi.json'))
+  const parsedData = JSON.parse(rawData)
+
+  const actions = {
+    list: () => parsedData
+  }
+  const state = {}
+  const store = new Vuex.Store({
+    modules: {
+      people: {
+          namespaced: true,
+          state,
+          mutations: {
+            results: (state, results = []) => (state.results = results)
+          },
+          actions,
+      },
+    }
+  })
+
   const wrapper = mount(Home, { store, localVue })
-  expect(wrapper.html()).toMatchSnapshot()
+
+  wrapper.vm.$nextTick(() => {
+    expect(wrapper.html()).toMatchSnapshot()
+    done()
+  })
 })
